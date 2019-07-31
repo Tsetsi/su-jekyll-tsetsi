c4986eca65d641a95ed6d27cfbd435f03c6564b6
Tsetsi <tdmax@mail.bg>
2019-07-31 13:21:28 +0300

    Added automatic build with Deploy function under gulp

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,9 @@ gulp.task("jekyll", function() {
     return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit", shell: true});
 })
 
+
+
+
 gulp.task("watch", function(){
 
     browserSync.init({
@@ -45,4 +48,5 @@ gulp.task("watch", function(){
 
 });
 
+gulp.task("deploy", gulp.series('jekyll', 'css'));
 gulp.task("default", gulp.series('jekyll', 'css', 'watch'));
